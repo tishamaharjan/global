b35facc964eba12f67687e54c0749d5bb635a569
tishamaharjan <139973058+tishamaharjan@users.noreply.github.com>
2025-07-02 10:52:22 +0545

    fix(app): fixed svg warnings

--- apps/website/src/assets/icons/Menu.tsx
+++ apps/website/src/assets/icons/Menu.tsx
@@ -9,41 +9,41 @@ const HamMenu = ({ className = "w-9 h-9" }) => {
         xmlns="http://www.w3.org/2000/svg"
         className={className}
       >
-        <g clip-path="url(#clip0_642_10605)">
+        <g clipPath="url(#clip0_642_10605)">
           <rect
             width="48"
             height="48"
             rx="8"
             fill="#191919"
-            fill-opacity="0.8"
+            fillOpacity="0.8"
           />
           <path
             opacity="0.1"
             d="M6.32317 49V-1M13.2156 49V-1M20.108 49V-1M27.0003 49V-1M33.8927 49V-1M40.7851 49V-1M-1 6.33794H49M-1 13.2305H49M-1 20.123H49M-1 27.0155H49M-1 33.908H49M-1 40.8005H49"
             stroke="white"
-            stroke-opacity="0.302"
-            stroke-width="0.673077"
+            strokeOpacity="0.302"
+            strokeWidth="0.673077"
           />
           <path
             d="M12 24H36"
             stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M12 16H36"
             stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M12 32H36"
             stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <mask id="path-6-inside-1_642_10605" fill="white">
             <path d="M0 8C0 3.58172 3.58172 0 8 0H40C44.4183 0 48 3.58172 48 8V40C48 44.4183 44.4183 48 40 48H8C3.58172 48 0 44.4183 0 40V8Z" />
