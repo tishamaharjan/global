1d49e50a961b2317d7e946c26403019101a0c3a1
tishamaharjan <139973058+tishamaharjan@users.noreply.github.com>
2025-07-02 21:59:00 +0545

    feat(app): implementing toast

--- apps/website/src/components/sections/Ideas.tsx
+++ apps/website/src/components/sections/Ideas.tsx
@@ -3,69 +3,79 @@ import { useTransition } from "react";
 import { Button } from "../Button";
 import { Input } from "../Input";
 import { Textarea } from "../Textarea";
+import { Toaster } from "@workspace/ui/components/sonner";
+import { toast } from "sonner";
 
 const Ideas = () => {
   const [isPending, setIsPending] = useTransition();
   return (
-    <section className="bg-contactbg border border-statbg py-[36px] px-[32px] rounded-2xl">
-      <div>
-        <span className="font-bold text-whitetext text-2xl">Got Ideas?</span>
-        <form
-          action={async (formData) => {
-            setIsPending(async () => {
-              await actions.send(formData);
-            });
-          }}
-          className="space-y-6 mt-5"
-        >
-          <div className="grid grid-cols-1 gap-4">
-            <div className="flex gap-3">
-              <Input
-                id="name"
-                name="name"
-                placeholder="Name"
-                required
-                className="py-[16px] px-[24px] h-[55px] content-between bg-darkinput bg-darkinput lg:bg-iconborder border-contactborder text-white placeholder:text-placeholdertext rounded-lg"
-              />
+    <>
+      <section className="bg-contactbg border border-statbg py-[36px] px-[32px] rounded-2xl">
+        <div>
+          <span className="font-bold text-whitetext text-2xl">Got Ideas?</span>
+          <form
+            action={async (formData) => {
+              setIsPending(async () => {
+                try {
+                  await actions.send(formData);
+                  toast.success("Thank you for sharing your idea!");
+                } catch (error) {
+                  toast.error("There was an error. Please try again.");
+                }
+              });
+            }}
+            className="space-y-6 mt-5"
+          >
+            <div className="grid grid-cols-1 gap-4">
+              <div className="flex gap-3">
+                <Input
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  required
+                  className="py-[16px] px-[24px] h-[55px] content-between bg-darkinput bg-darkinput lg:bg-iconborder border-contactborder text-white placeholder:text-placeholdertext rounded-lg"
+                />
 
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Email"
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  required
+                  className="py-[16px] px-[24px] h-[55px] content-between bg-darkinput bg-darkinput lg:bg-iconborder border-contactborder text-white placeholder:text-placeholdertext rounded-lg"
+                />
+              </div>
+            </div>
+
+            <div className="flex gap-3">
+              <Textarea
+                id="message"
+                name="message"
+                placeholder="Message"
                 required
-                className="py-[16px] px-[24px] h-[55px] content-between bg-darkinput bg-darkinput lg:bg-iconborder border-contactborder text-white placeholder:text-placeholdertext rounded-lg"
+                className="h-[104px] py-[16px] px-[24px] bg-darkinput lg:bg-iconborder border-contactborder text-white placeholder:text-placeholdertext rounded-lg"
               />
             </div>
-          </div>
 
-          <div className="flex gap-3">
-            <Textarea
-              id="message"
-              name="message"
-              placeholder="Message"
-              required
-              className="h-[104px] py-[16px] px-[24px] bg-darkinput lg:bg-iconborder border-contactborder text-white placeholder:text-placeholdertext rounded-lg"
-            />
-          </div>
-
-          <Button
-            type="submit"
-            className="cursor-pointer w-full h-[51px] text-darkergreen hover:bg-darkergreen px-[16px] py-[12px] mt-10 rounded-lg"
-            style={{
-              background: "rgba(25, 25, 25, 0.80)",
-              borderRight:
-                "2px solid var(--color-grey-9250, rgba(234, 234, 234, 0.50))",
-              borderBottom:
-                "2px solid var(--color-grey-9250, rgba(234, 234, 234, 0.50))",
-            }}
-            disabled={isPending}
-          >
-            {isPending ? "Sending..." : "Send"}
-          </Button>
-        </form>
-      </div>
-    </section>
+            <Button
+              type="submit"
+              className="cursor-pointer w-full h-[51px] text-darkergreen hover:bg-darkergreen px-[16px] py-[12px] mt-10 rounded-lg"
+              style={{
+                background: "rgba(25, 25, 25, 0.80)",
+                borderRight:
+                  "2px solid var(--color-grey-9250, rgba(234, 234, 234, 0.50))",
+                borderBottom:
+                  "2px solid var(--color-grey-9250, rgba(234, 234, 234, 0.50))",
+              }}
+              disabled={isPending}
+            >
+              {isPending ? "Sending..." : "Send"}
+            </Button>
+          </form>
+        </div>
+      </section>
+      <Toaster />
+    </>
   );
 };
 
--- apps/website/src/components/sections/Newsletter.tsx
+++ apps/website/src/components/sections/Newsletter.tsx
@@ -2,55 +2,103 @@ import Component1 from "@/assets/icons/Component1";
 import { Button } from "../Button";
 import { Input } from "../Input";
 import ColoredVector from "../../../public/ColoredVectorBg.svg";
+import { useState } from "react";
+import { Toaster } from "@workspace/ui/components/sonner";
+import { toast } from "sonner";
 
 const Newsletter = () => {
-  return (
-    <section
-      className="bg-contactbg border border-statbg py-[80px] px-[36px] relative overflow-hidden items-start rounded-2xl"
-      style={{
-        backgroundImage: `url(${ColoredVector.src})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100%",
-      }}
-    >
-      <div className=" text-center">
-        <h2 className="text-[38px] font-bold mb-6 md:hidden text-whitetext">
-          Join Our <br />
-          Newsletter
-        </h2>
-        <h2 className="text-5xl font-bold mb-6 hidden md:block text-whitetext">
-          Join Our Newsletter
-        </h2>
-        <p className="text-white/60 mb-8 text-[18px]">
-          Join our newsletter community for exclusive updates, industry
-          insights, special offers, and valuable tips.
-        </p>
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-        <div className="flex sm:flex-row max-w-xl mx-auto border-white/10 rounded-[25px] justify-center">
-          <div
-            className="flex bg-iconborder rounded-[34px] py-1 pr-2 pl-6 w-[436px] h-[60px] items-center"
-            style={{ boxShadow: "0px 0px 0px 1px border) inset" }}
-          >
-            <Input
-              type="email"
-              placeholder="example@gs.com"
-              className=" bg-iconborder mr-auto max-md:w-[160px] border-none md:w-[436px] text-[16px] font-normal placeholder:text-white/40 h-10 focus-visible:ring-0 focus-visible:ring-offset-0"
-            />
-            <Button
-              className="cursor-pointer border-r-[2px] border-b-[4px] border-greenbackground bg-yellowgreen hover:bg-darkergreen text-black px-4 py-[10px] h-[52px] w-[111px] text-[18px] rounded-[25px] animate-fade-in"
-              style={{
-                animationDelay: "0.3s",
-              }}
+  const url =
+    "https://script.google.com/macros/s/AKfycbxBb2C8b8geYvjxTcYPQCqhNr-vlkYfkqdXMqXcwpmyGeVm77_2oQg02FPupTWtmbg/exec";
+
+  const subscribeToNewsletter = async (email: string, url: string) => {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ email }),
+    });
+    return response.json();
+  };
+
+  const JoinUsEmail = async () => {
+    try {
+      setIsLoading(true);
+      const res = await subscribeToNewsletter(email, url);
+      if (res.result === "Success") {
+        toast.success("Thank you for joining our newsletter!");
+        setIsLoading(false);
+        setEmail("");
+      } else {
+        toast.error("There was an error. Please try again.");
+      }
+      console.log(res, "Response from Google Script");
+    } catch (error) {
+      console.error("Error joining newsletter:", error);
+      toast.error("There was an error. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <section
+        className="bg-contactbg border border-statbg py-[80px] px-[36px] relative overflow-hidden items-start rounded-2xl"
+        style={{
+          backgroundImage: `url(${ColoredVector.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100%",
+        }}
+      >
+        <div className=" text-center">
+          <h2 className="text-[38px] font-bold mb-6 md:hidden text-whitetext">
+            Join Our <br />
+            Newsletter
+          </h2>
+          <h2 className="text-5xl font-bold mb-6 hidden md:block text-whitetext">
+            Join Our Newsletter
+          </h2>
+          <p className="text-white/60 mb-8 text-[18px]">
+            Join our newsletter community for exclusive updates, industry
+            insights, special offers, and valuable tips.
+          </p>
+
+          <div className="flex sm:flex-row max-w-xl mx-auto border-white/10 rounded-[25px] justify-center">
+            <div
+              className="flex bg-iconborder rounded-[34px] py-1 pr-2 pl-6 w-[436px] h-[60px] items-center"
+              style={{ boxShadow: "0px 0px 0px 1px border) inset" }}
             >
-              Join
-              <div className="ml-1 w-[32px] h-[32px]">
-                <Component1 />
-              </div>
-            </Button>
+              <Input
+                type="email"
+                placeholder="example@gs.com"
+                className=" bg-iconborder mr-auto max-md:w-[160px] border-none md:w-[436px] text-[16px] font-normal placeholder:text-white/40 h-10 focus-visible:ring-0 focus-visible:ring-offset-0"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Button
+                className="cursor-pointer border-r-[2px] border-b-[4px] border-greenbackground bg-yellowgreen hover:bg-darkergreen text-black px-4 py-[10px] h-[52px] w-[111px] text-[18px] rounded-[25px] animate-fade-in"
+                style={{
+                  animationDelay: "0.3s",
+                }}
+                onClick={JoinUsEmail}
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <div className="animate-spin rounded-full border-2 border-black w-5 h-5 border-t-transparent" />
+                ) : (
+                  "Join"
+                )}
+                <div className="ml-1 w-[32px] h-[32px]">
+                  <Component1 />
+                </div>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Toaster />
+    </>
   );
 };
 
